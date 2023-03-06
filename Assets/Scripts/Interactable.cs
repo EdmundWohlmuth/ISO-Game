99d939176fd626af8eadaecdb51c9b094b040015
Edmund Wohlmuth <edmundwoh@gmail.com>
2023-03-06 07:47:02 -0400

    added water asset

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public void OnInteract()
+    {
+
+    }
+
+    public void OnGainFocus()
+    {
+
+    }
+
+    public void OnLooseFocus()
+    {
+
+    }
+}
--- Assets/Scripts/TempController.cs
+++ Assets/Scripts/TempController.cs
@@ -6,21 +6,60 @@ public class TempController : MonoBehaviour
 {
     float verticalSpeed = 15f;
     float horizontalSpeed = 15f;
-    public GameObject Cam;
+    Camera cam;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        cam = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // camera movement
+        Movement();
+        CameraScroll();
+
+        // raycast to curosr pos
+        MouseToPos();
+    }
+
+    void Movement()
+    {
         verticalSpeed = Input.GetAxis("Vertical") * Time.deltaTime;
-        transform.Translate(verticalSpeed * 5f, 0, 0);
+        transform.Translate(verticalSpeed * 8f, 0, 0);
 
         horizontalSpeed = Input.GetAxis("Horizontal") * Time.deltaTime;
-        transform.Translate(0, 0, horizontalSpeed * -5f);
+        transform.Translate(0, 0, horizontalSpeed * -8f);
+    }
+    void CameraScroll()
+    {
+        if (Input.mouseScrollDelta.y >= 1)
+        {
+            cam.orthographicSize++;
+
+            if (cam.orthographicSize >= 25)
+            {
+                cam.orthographicSize = 25;
+            }
+        }
+        else if (Input.mouseScrollDelta.y <= -1)
+        {
+            cam.orthographicSize--;
+
+            if (cam.orthographicSize <= 5)
+            {
+                cam.orthographicSize = 5;
+            }
+        }
+    }
+
+    void MouseToPos()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = 10f;
+        mousePos = cam.ScreenToWorldPoint(mousePos);
+        Debug.DrawRay(transform.position, mousePos - transform.position, Color.blue);
     }
 }
